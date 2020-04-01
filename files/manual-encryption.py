7a81ca9f695fc914e0904a3942ebed76b405f626
manalito <aurelien.siu@heig-vd.ch>
2020-04-01 21:55:26 +0200

    last edit step2

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -12,31 +12,45 @@ from scapy.all import *
 import binascii
 import rc4
 
-
 #Cle wep AA:AA:AA:AA:AA
-key='\xaa\xaa\xaa\xaa\xaa'
+key=b'\xaa\xaa\xaa\xaa\xaa'
 
-#Lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient
+def rc4(s, val):
+    l = len(val)
+    buf = bytearray(l)
+    i = 0
+    j = 0
+    idx = 0
+    while idx < l:
+        i = (i + 1) & 0xff
+        j = (j + s[i]) & 0xff
+        s[i], s[j] = s[j], s[i]
+        k = s[(s[i] + s[j]) & 0xff]
+        buf[idx] = (ord(val[idx])) ^ k
+        idx = idx + 1
+    return str(buf)
+
+# Lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient
 arp = rdpcap('arp.cap')[0]
 
-#message à crypter 
+# message à chiffrer
 plaintext = "Welcome to lab WEP of SWI"
 
+# rc4 seed est composé de IV+clé
+seed = arp.iv + key
 
-#rc4 seed est composé de IV+clé
-seed = arp.iv+key
+# Calcul de l'ICV du plaintext
+icv = binascii.crc32(plaintext.encode()) & 0xffffffff
 
-#Calcul de l'ICV du plaintext
-icv = binascii.crc32(plaintext) & 0xffffffff
-
-#Conversion de l'icv en format int 
+# Conversion de l'icv en format int 
 icv = struct.pack('I', icv)
 
-#Concaténation du message et de l'ICV
-plaintext = plaintext + icv
+
+# Concaténation du message et de l'ICV
+plaintext1 = plaintext + str(icv)
 
 # Calcul du frame body en faisant keystream xor message_clear
-cipher= rc4.rc4crypt(plaintext, seed)
+cipher= rc4(seed, plaintext1)
 
 #Remplacement des champs wepdata par le message crypté et de l'icv
 arp.wepdata = cipher[:-4]
@@ -44,8 +58,8 @@ arp.wepdata = cipher[:-4]
 
 
 # Affichage de quelques information
-print 'Text: ' + arp.wepdata.encode("hex")
-print 'icv:  ' + icv.encode("hex")
+print ('Text: ' + arp.wepdata.encode("hex"))
+print ('icv:  ' + icv.encode("hex"))
 
 #Ecriture de la nouvelle trame dans le fichier arp1.cap
 wrpcap('arp1.cap', arp)
